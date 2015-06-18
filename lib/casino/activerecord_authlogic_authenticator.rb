62d7742fdcae84c8b41a8bc48afb3c7a9b8360f8
Marcin Ożóg <marcin.ozog@vml.com>
2015-06-18 10:13:38 +0200

    Refactor authenticator, validate options

--- lib/casino/activerecord_authlogic_authenticator.rb
+++ lib/casino/activerecord_authlogic_authenticator.rb
@@ -1,6 +1,5 @@
 require 'active_record'
 require 'authlogic'
-require 'phpass'
 
 class CASino::ActiveRecordAuthLogicAuthenticator
 
@@ -8,29 +7,13 @@ class CASino::ActiveRecordAuthLogicAuthenticator
     self.abstract_class = true
   end
 
-    def initialize(options)
-    if !options.respond_to?(:deep_symbolize_keys)
-      raise ArgumentError, "When assigning attributes, you must pass a hash as an argument."
-    end
-    @options = options.deep_symbolize_keys
-    raise ArgumentError, "Table name is missing" unless @options[:table]
-    if @options[:model_name]
-      model_name = @options[:model_name]
-    else
-      model_name = @options[:table]
-      if @options[:connection].kind_of?(Hash) && @options[:connection][:database]
-        model_name = "#{@options[:connection][:database].gsub(/[^a-zA-Z]+/, '')}_#{model_name}"
-      end
-      model_name = model_name.classify
-    end
+  def initialize(options)
+    set_options options
+    validate_options
+    model_name = get_model_name
+    model_name = classify_table_model_name model_name if model_name == options[:table]
     model_class_name = "#{self.class.to_s}::#{model_name}"
-    eval <<-END
-      class #{model_class_name} < AuthDatabase
-        self.table_name = "#{@options[:table]}"
-        self.inheritance_column = :_type_disabled
-      end
-    END
-
+    load_class model_class_name
     @model = model_class_name.constantize
     @model.establish_connection @options[:connection]
   end
@@ -58,11 +41,53 @@ class CASino::ActiveRecordAuthLogicAuthenticator
 
   private
 
-  def user_data(user)
+  def set_options options
+    if !options.respond_to?(:deep_symbolize_keys)
+      raise ArgumentError, "When assigning attributes, you must pass a hash as an argument."
+    end
+    @options = options.deep_symbolize_keys
+  end
+
+  def validate_options
+    raise ArgumentError, "Table name is missing" if @options[:table].blank?
+    if @options[:encryptor] && !(@options[:encryptor].in? allowed_encryptors)
+      raise ArgumentError, "Bad encryptor. Available encryptors: #{allowed_encryptors.join(',')}"
+    end
+  end
+
+  def allowed_encryptors
+    ["Sha512", "AES256", "BCrypt", "MD5", "Sha1"]
+  end
+
+  def get_model_name
+    if @options[:model_name]
+      @options[:model_name]
+    else
+      @options[:table]
+    end
+  end
+
+  def classify_table_model_name model_name
+    if @options[:connection].kind_of?(Hash) && @options[:connection][:database]
+      model_name = "#{@options[:connection][:database].gsub(/[^a-zA-Z]+/, '')}_#{model_name}"
+    end
+    model_name.classify
+  end
+
+  def load_class model_class_name
+    eval <<-END
+        class #{model_class_name} < AuthDatabase
+          self.table_name = "#{@options[:table]}"
+          self.inheritance_column = :_type_disabled
+        end
+      END
+  end
+
+  def user_data user
     { username: user.send(@options[:username_column]), extra_attributes: extra_attributes(user) }
   end
 
-  def valid_password?(password, crypted_password, password_salt)
+  def valid_password? password, crypted_password, password_salt
     return false if crypted_password.blank?
     tokens = [password, password_salt]
     encryptor.matches?(crypted_password, tokens)
